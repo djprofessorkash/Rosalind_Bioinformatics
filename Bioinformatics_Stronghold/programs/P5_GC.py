ee837a633c7b1982640e6f30f1386950b0547138
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-02-21 23:34:54 -0800

    Updated P5 (GC) program with problem parameters

--- Bioinformatics_Stronghold/programs/P5_GC.py
+++ Bioinformatics_Stronghold/programs/P5_GC.py
@@ -2,13 +2,38 @@
 NAME:               Computing GC Content (Bioinformatics Stronghold)
 CONTRIBUTOR:        Aakash Sudhakar
 
-PROBLEM:            ...
+PROBLEM:            The GC-content of a DNA string is given by the percentage of symbols in the string 
+                    that are 'C' or 'G'. For example, the GC-content of "AGCTATAG" is 37.5%. Note that the 
+                    reverse complement of any DNA string has the same GC-content.
 
-DATASET:            ...
-OUTPUT:             ...
+                    DNA strings must be labeled when they are consolidated into a database. A commonly used 
+                    method of string labeling is called FASTA format. In this format, the string is introduced 
+                    by a line that begins with '>', followed by some labeling information. Subsequent lines 
+                    contain the string itself; the first line to begin with '>' indicates the label of 
+                    the next string.
 
-SAMPLE DATASET:     ...
-SAMPLE OUTPUT:      ...
+                    In Rosalind's implementation, a string in FASTA format will be labeled by 
+                    the ID "Rosalind_xxxx", where "xxxx" denotes a four-digit code between 0000 and 9999.
+
+DATASET:            At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+OUTPUT:             The ID of the string having the highest GC-content, followed by the GC-content 
+                    of that string. Rosalind allows for a default error of 0.001 in all decimal answers 
+                    unless otherwise stated.
+
+SAMPLE DATASET:     >Rosalind_6404
+                    CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+                    TCCCACTAATAATTCTGAGG
+
+                    >Rosalind_5959
+                    CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+                    ATATCCATTTGTCAGCAGACACGC
+
+                    >Rosalind_0808
+                    CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+                    TGGGAACCTGCGGGCAGTAGGTGGAAT
+                    
+SAMPLE OUTPUT:      Rosalind_0808
+                    60.919540
 
 STATUS:             Incomplete.
 """
