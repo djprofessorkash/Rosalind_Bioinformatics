1a65d2d9585fd09c6992da27303d57e8cf84f3f0
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-02-21 23:31:59 -0800

    Created file for P5 (GC)

--- Bioinformatics_Stronghold/programs/P5_GC.py
+++ Bioinformatics_Stronghold/programs/P5_GC.py
@@ -1,1 +1,21 @@
-# whoo
+"""
+NAME:               Computing GC Content (Bioinformatics Stronghold)
+CONTRIBUTOR:        Aakash Sudhakar
+
+PROBLEM:            ...
+
+DATASET:            ...
+OUTPUT:             ...
+
+SAMPLE DATASET:     ...
+SAMPLE OUTPUT:      ...
+
+STATUS:             Incomplete.
+"""
+
+
+def main():
+    """ ... """
+
+if __name__ == "__main__":
+    main()
