6658fb23f01c3d86f486ce931c33f36c9953fa17
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-03-03 01:43:18 -0800

    Completed dictionary-based implementation of P8 challenge

--- Bioinformatics_Stronghold/programs/P8_PROT.py
+++ Bioinformatics_Stronghold/programs/P8_PROT.py
@@ -11,6 +11,10 @@ PROBLEM:            The 20 commonly occurring amino acids are abbreviated by usi
                     The RNA codon table dictates the details regarding the encoding of 
                     specific codons into the amino acid alphabet.
 
+                    NOTE: Script contains two implementations: one using a dictionary holding
+                    RNA-to-protein conversions as key-value pairs, and other using tree
+                    traversals to produce codons per individual base translations. 
+
 DATASET:            An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
 
 OUTPUT:             The protein string encoded by s.
@@ -18,28 +22,51 @@ OUTPUT:             The protein string encoded by s.
 SAMPLE DATASET:     AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA
 SAMPLE OUTPUT:      MAMAPRTEINSTRING
 
-STATUS:             Pending.
+STATUS:             Submitted, additional work available.
 """
 
 
-def translation_tree():
+def protein_translation_with_table(data):
+    translation_table = {
+        "UUU": "F",      "CUU": "L",      "AUU": "I",      "GUU": "V",
+        "UUC": "F",      "CUC": "L",      "AUC": "I",      "GUC": "V",
+        "UUA": "L",      "CUA": "L",      "AUA": "I",      "GUA": "V",
+        "UUG": "L",      "CUG": "L",      "AUG": "M",      "GUG": "V",
+        "UCU": "S",      "CCU": "P",      "ACU": "T",      "GCU": "A",
+        "UCC": "S",      "CCC": "P",      "ACC": "T",      "GCC": "A",
+        "UCA": "S",      "CCA": "P",      "ACA": "T",      "GCA": "A",
+        "UCG": "S",      "CCG": "P",      "ACG": "T",      "GCG": "A",
+        "UAU": "Y",      "CAU": "H",      "AAU": "N",      "GAU": "D",
+        "UAC": "Y",      "CAC": "H",      "AAC": "N",      "GAC": "D",
+        "UAA": "Stop",   "CAA": "Q",      "AAA": "K",      "GAA": "E",
+        "UAG": "Stop",   "CAG": "Q",      "AAG": "K",      "GAG": "E",
+        "UGU": "C",      "CGU": "R",      "AGU": "S",      "GGU": "G",
+        "UGC": "C",      "CGC": "R",      "AGC": "S",      "GGC": "G",
+        "UGA": "Stop",   "CGA": "R",      "AGA": "R",      "GGA": "G",
+        "UGG": "W",      "CGG": "R",      "AGG": "R",      "GGG": "G"
+    }
+    protein_chain = str()
+    for iterator in range(0, len(data), 3):
+        codon = data[iterator:iterator+3]
+        if translation_table[codon] == "Stop":
+            return protein_chain
+        protein_chain += translation_table[codon]
+
+def protein_translation_with_tree(data):
     pass
 
 def main():
     # NOTE: Requires being in parent repo ('pwd' must return up to directory '/Rosalind_Bioinformatics/Bioinformatics_Stronghold')
-    # FILEPATHREAD = "./datasets/P8_PROT-dataset.txt"
-    FILEPATHREAD = "./datasets/P8_example.txt"
+    FILEPATHREAD = "./datasets/P8_PROT-dataset.txt"
     FILEPATHWRITE = "./outputs/P8_PROT-output.txt"
 
     # Reads text data from raw dataset as single-line array of characters
     with open(FILEPATHREAD, "r") as fr:
         data = fr.read()
 
-    
-
     # Creates output file and writes appropriate response to file and notifies user
     with open(FILEPATHWRITE, "w") as fw:
-        fw.write()
+        fw.write(str(protein_translation_with_table(data)))
 
     return print("\nThe RNA Translation dataset has been processed and the appropriate output has been saved to {}.\n".format(FILEPATHWRITE))
 
