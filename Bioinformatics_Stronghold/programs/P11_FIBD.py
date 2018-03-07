4b0432715c556597c3d985de55743edb29cb5129
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-03-06 18:26:56 -0800

    P11 script works for small inputs but times out at larger inputs, must enhance

--- Bioinformatics_Stronghold/programs/P11_FIBD.py
+++ Bioinformatics_Stronghold/programs/P11_FIBD.py
@@ -25,30 +25,33 @@ STATUS:             Pending.
 """
 
 
-from math import sqrt
-
-# def k_recurrence_relation(n, k):
-#     """ Calculates fibonacci sequence and returns appropriate result. """
-#     if n < 40: 
-#         if n == 0: 
-#             return 0
-#         elif n == 1: 
-#             return 1
-#         else:
-#             # Recursive functional call for Fibonacci calculation
-#             return k_recurrence_relation(n - 1, k) + k * k_recurrence_relation(n - 2, k)
-#     else:
-#         # Approximates fibonacci sequence result for runtime
-#         return round((1 + sqrt(5))**n - (1 - sqrt(5))**n) / (2**n*sqrt(5))
+# NOTE: It always takes two months (generations) for an infant pair to grow up and have kids.
 
 def mortal_recurrence_relation(n, m):
-    e = mc^2
-    pass
+    """ Calculates fibonacci sequence with mortality rate m and returns total offspring
+    after n months. """
+    if n <= 100 or m <= 20:
+        if n == 0:
+            # Standard Fibonacci Boundary Condition
+            return 0
+        elif n == 1:
+            # Standard Fibonacci Boundary Condition
+            return 1
+        elif n <= m:
+            # Standard Fibonacci General Logic
+            return mortal_recurrence_relation(n - 1, m) + mortal_recurrence_relation(n - 2, m)
+        elif n == m + 1:
+            # Account for mortality with f(n - (m + 1), m) where n == m, therefore f == -1
+            return mortal_recurrence_relation(n - 1, m) + mortal_recurrence_relation(n - 2, m) - 1
+        else:
+            # Account for general mortality with f(n - (m + 1), m) where n != m
+            return mortal_recurrence_relation(n - 1, m) + mortal_recurrence_relation(n - 2, m) - mortal_recurrence_relation(n - (m + 1), m)
+    else:
+        raise ValueError("\n\nFUNCTION PARAMETERS ARE TOO LARGE TO HANDLE.\n\nn <= 100 (CURRENT: n = {})\nm <= 20 (CURRENT: m = {})\n".format(n, m))
 
 def main():
     # NOTE: Requires being in parent repo ('pwd' must return up to directory '/Rosalind_Bioinformatics/Bioinformatics_Stronghold')
-    # FILEPATHREAD = "./datasets/P11_FIBD-dataset.txt"
-    FILEPATHREAD = "./datasets/P11-sample.txt"
+    FILEPATHREAD = "./datasets/P11_FIBD-dataset.txt"
     FILEPATHWRITE = "./outputs/P11_FIBD-output.txt"
 
     # Reads text data from raw dataset as single-line array of characters
@@ -56,8 +59,8 @@ def main():
         data = fr.read().split(" ")
 
     # Creates output file and writes appropriate response to file and notifies user
-    # with open(FILEPATHWRITE, "w") as fw:
-    #     fw.write()
+    with open(FILEPATHWRITE, "w") as fw:
+        fw.write(str(mortal_recurrence_relation(int(data[0]), int(data[1]))))
 
     return print("\nThe Mortal Fibonacci dataset has been processed and the appropriate output has been saved to {}.\n".format(FILEPATHWRITE))
 
