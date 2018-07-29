ba15a46a38902dfc0ec7ab0d0f37f1dd8eafe76d
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-07-29 09:57:07 -0700

    Attempting overlap graphs solution

--- Bioinformatics_Stronghold/programs/P12_GRPH.py
+++ Bioinformatics_Stronghold/programs/P12_GRPH.py
@@ -45,15 +45,36 @@ SAMPLE OUTPUT:      Rosalind_0498 Rosalind_2391
 STATUS:             Pending.
 """
 
+def parse_fasta_data(dataset):
+    """  """
+    parsed_data, strings = list(), dataset.strip().split(">")
+
+    for string in strings:
+        if len(string):
+            components = string.split()
+            key, value = components[0], "".join(components[1:])
+            parsed_data.append((key, value))
+    return parsed_data
+
+def overlap_graph(data, n):
+    """  """
+    graph = list()
+    for key0, val0 in data:
+        for key1, val1 in data:
+            if key0 != key1 and val0.endswith(val1[:n]):
+                graph.append((key0, key1))
+
 def main():
     # NOTE: Requires being in parent repo ('pwd' must return up to directory '/Rosalind_Bioinformatics/Bioinformatics_Stronghold')
-    FILEPATHREAD = "./datasets/P12_sample.txt"
+    FILEPATHREAD = "./datasets/P12_GRPH-sample.txt"
     # FILEPATHREAD = "./datasets/P12_GRPH-dataset.txt"
     FILEPATHWRITE = "./outputs/P12_GRPH-output.txt"
 
     # Reads text data from raw dataset as single-line array of characters
     with open(FILEPATHREAD, "r") as fr:
-        data = fr.read()
+        data = parse_fasta_data(fr.read())
+
+    graph = overlap_graph(data, 3)
 
     # Creates output file and writes appropriate response to file and notifies user
     with open(FILEPATHWRITE, "w") as fw:
@@ -63,3 +84,40 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+"""
+def overlap_graph(fasta, n):
+    results = []
+
+    dna = parse_fasta(fasta)
+
+    for k1, v1 in dna:
+        for k2, v2 in dna:
+            if k1 != k2 and v1.endswith(v2[:n]):
+                results.append((k1, k2))
+
+    return results
+
+
+if __name__ == "__main__":
+
+    small_dataset = '''
+                    >Rosalind_0498
+                    AAATAAA
+                    >Rosalind_2391
+                    AAATTTT
+                    >Rosalind_2323
+                    TTTTCCC
+                    >Rosalind_0442
+                    AAATCCC
+                    >Rosalind_5013
+                    GGGTGGG
+                    '''
+
+    large_dataset = open('datasets/rosalind_grph.txt').read()
+
+    for edge in overlap_graph(large_dataset, 3):
+        print edge[0], edge[1]
+"""
--- Bioinformatics_Stronghold/programs/P4_FIB.py
+++ Bioinformatics_Stronghold/programs/P4_FIB.py
@@ -39,10 +39,8 @@ from math import sqrt
 def k_recurrence_relation(n, k):
     """ Calculates fibonacci sequence and returns appropriate result to main() """
     if n < 40: 
-        if n == 0: 
-            return 0
-        elif n == 1: 
-            return 1
+        if n == 0 or n == 1:
+            return n
         else: 
             return k_recurrence_relation(n - 1, k) + k * k_recurrence_relation(n - 2, k)    # Recursive functional call for Fibonacci calculation
     else:
