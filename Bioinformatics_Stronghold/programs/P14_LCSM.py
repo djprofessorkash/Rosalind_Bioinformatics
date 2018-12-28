1bdcc6f9fbcff989d739b81892dd2eeba21c4dde
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-12-27 23:16:05 -0600

    My Suffix LCS code did not pass Rosalind's tests and I am heartbroken

--- Bioinformatics_Stronghold/programs/P14_LCSM.py
+++ Bioinformatics_Stronghold/programs/P14_LCSM.py
@@ -28,7 +28,7 @@ SAMPLE DATASET:     >Rosalind_1
 
 SAMPLE OUTPUT:      AC
 
-STATUS:             Submission failed: INCORRECT.
+STATUS:             Submission pending.
 """
 
 from supplements import SUPP_STree as STree
@@ -42,21 +42,20 @@ def _parse_fasta_data(raw_data):
 
 def main():
     # NOTE: Requires being in parent repo ('pwd' must return up to directory '/Rosalind_Bioinformatics/Bioinformatics_Stronghold')
-    FILEPATHREAD = "./datasets/P14_LCSM-sample.txt"
-    # FILEPATHREAD = "./datasets/P14_LCSM-dataset.txt"
+    # FILEPATHREAD = "./datasets/P14_LCSM-sample.txt"
+    FILEPATHREAD = "./datasets/P14_LCSM-dataset.txt"
     FILEPATHWRITE = "./outputs/P14_LCSM-output.txt"
 
     # Reads text data from raw dataset as single-line array of characters
     with open(FILEPATHREAD, "r") as fr:
         data = fr.readlines()
 
+    # Creates generalized Suffix tree (GST) to hold genomic text structure
     generalized_suffix_tree = STree.Suffix_Tree(_parse_fasta_data(data))
-    print("\n> Parsed Data: " + str(_parse_fasta_data(data)))
-    print("\n> Shared Motif: " + generalized_suffix_tree.determine_LCS())
 
     # Creates output file and writes appropriate response to file and notifies user
-    # with open(FILEPATHWRITE, "w") as fw:
-    #     fw.write(find_longest_common_substring(_parse_fasta_data(data)))
+    with open(FILEPATHWRITE, "w") as fw:
+        fw.write(str(generalized_suffix_tree.determine_LCS()))
 
     return print("\nThe Shared Motif dataset has been processed and the appropriate output has been saved to {}.\n".format(FILEPATHWRITE[2:]))
 
