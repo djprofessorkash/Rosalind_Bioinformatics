3601a9378a3725624a709751ec07924fed11e3f0
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-01-04 20:40:21 -0500

    Progressing on P22 with passed sample data case

--- Bioinformatics_Stronghold/programs/P22_SPLC.py
+++ Bioinformatics_Stronghold/programs/P22_SPLC.py
@@ -24,19 +24,68 @@ SAMPLE OUTPUT:      MVYIADKQHVASREAYGHMFKVCA
 STATUS:             Pending.
 """
 
+def _restructure_dataset(dataset, GENOME_LABELS):
+    """ Restructures dataset into dictionary of main sequence and intron subsequences. """
+    dataset_formatted = dict()
+    dataset_formatted[GENOME_LABELS[0]], dataset_formatted[GENOME_LABELS[1]] = dataset[0], dataset[1:]
+    return dataset_formatted
+
+# TODO: Improve function by conserving raw sequence without duplicating original data.
+def genome_splicer(dataset):
+    """ Detects introns across genomic sequence and splices them out. """
+    GENOME_LABELS= ("sequence", "introns", "proteins")
+    genomic_data = _restructure_dataset(dataset, GENOME_LABELS)
+    for intron in genomic_data[GENOME_LABELS[1]]:
+        if intron in genomic_data[GENOME_LABELS[0]]:
+            genomic_data[GENOME_LABELS[0]] = genomic_data[GENOME_LABELS[0]].replace(intron, "")
+    return genomic_data
+
+def protein_constructor(sequence):
+    """ Constructs protein chain from genomic data using amino acid lookup table. """
+    protein_sequence, CODON_LENGTH, STOP_CODON = str(), 3, "Stop"
+    for iterator in range(0, len(sequence), CODON_LENGTH):
+        protein = _codon_translator(sequence[iterator:iterator+CODON_LENGTH].replace("T", "U"))
+        if protein is STOP_CODON:
+            break
+        protein_sequence += protein
+    return protein_sequence
+
+def _codon_translator(codon):
+    """ Translates single codon using lookup table into effective amino acid. """
+    CODON_TRANSLATION_TABLE = {
+        "UUU": "F",      "CUU": "L",      "AUU": "I",      "GUU": "V",
+        "UUC": "F",      "CUC": "L",      "AUC": "I",      "GUC": "V",
+        "UUA": "L",      "CUA": "L",      "AUA": "I",      "GUA": "V",
+        "UUG": "L",      "CUG": "L",      "AUG": "M",      "GUG": "V",
+        "UCU": "S",      "CCU": "P",      "ACU": "T",      "GCU": "A",
+        "UCC": "S",      "CCC": "P",      "ACC": "T",      "GCC": "A",
+        "UCA": "S",      "CCA": "P",      "ACA": "T",      "GCA": "A",
+        "UCG": "S",      "CCG": "P",      "ACG": "T",      "GCG": "A",
+        "UAU": "Y",      "CAU": "H",      "AAU": "N",      "GAU": "D",
+        "UAC": "Y",      "CAC": "H",      "AAC": "N",      "GAC": "D",
+        "UAA": "Stop",   "CAA": "Q",      "AAA": "K",      "GAA": "E",
+        "UAG": "Stop",   "CAG": "Q",      "AAG": "K",      "GAG": "E",
+        "UGU": "C",      "CGU": "R",      "AGU": "S",      "GGU": "G",
+        "UGC": "C",      "CGC": "R",      "AGC": "S",      "GGC": "G",
+        "UGA": "Stop",   "CGA": "R",      "AGA": "R",      "GGA": "G",
+        "UGG": "W",      "CGG": "R",      "AGG": "R",      "GGG": "G"
+    }
+    if codon in CODON_TRANSLATION_TABLE.keys():
+        return CODON_TRANSLATION_TABLE[codon]
+
 def main():
     # NOTE: Requires being in parent repo ('pwd' must return up to directory '/Rosalind_Bioinformatics/Bioinformatics_Stronghold')
-    FILEPATHREAD = "./datasets/P22_sample.txt"
+    FILEPATHREAD = "./datasets/P22_SPLC-sample.txt"
     # FILEPATHREAD = "./datasets/P22_SPLC-dataset.txt"
     FILEPATHWRITE = "./outputs/P22_SPLC-output.txt"
 
     # Reads text data from raw dataset as single-line array of characters
     with open(FILEPATHREAD, "r") as fr:
-        data = fr.read()
-
+        data = [seq.strip() for seq in fr.readlines()[1::2]]
+    
     # Creates output file and writes appropriate response to file and notifies user
     with open(FILEPATHWRITE, "w") as fw:
-        fw.write(str(data))
+        fw.write(protein_constructor(genome_splicer(data)["sequence"]))
 
     return print("\nThe RNA Splicing dataset has been processed and the appropriate output has been saved to {}.\n".format(FILEPATHWRITE[2:]))
 
