67292f20261b8b484b0889a50b7ff12cbbd06224
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-01-05 22:11:53 -0500

    Unsuccessful with resolving P26 due to factorial calculation inaccuracy

--- Bioinformatics_Stronghold/programs/P26_PMCH.py
+++ Bioinformatics_Stronghold/programs/P26_PMCH.py
@@ -27,33 +27,46 @@ PROBLEM:            A matching in a graph G is a collection of edges of G for wh
                     to exist, S must have the same number of occurrences of 'A' as 'U' and 
                     the same number of occurrences of 'C' as 'G'.
 
-DATASET:            An RNA string s of length at most 80 bp having the same number of occurrences 
+DATASET:            An RNA string S of length at most 80 bp having the same number of occurrences 
                     of 'A' as 'U' and the same number of occurrences of 'C' as 'G'.
 
 OUTPUT:             The total possible number of perfect matchings of basepair edges 
-                    in the bonding graph of s.
+                    in the bonding graph of S.
 
 SAMPLE DATASET:     >Rosalind_23
                     AGCUAGUCAU
 
 SAMPLE OUTPUT:      12
 
-STATUS:             Pending.
+STATUS:             Submission failed.
 """
+
+from math import factorial as fact
+
+def calculate_perfect_matchings(sequence):
+    """ Calculates perfect matchings of basepair edges across RNA input string. """
+    gc_content, au_content = int(), int()
+    for base in sequence:
+        if base == "A":
+            au_content += 1
+        elif base == "G":
+            gc_content += 1
+    return fact(au_content) * fact(gc_content)
 
 def main():
     # NOTE: Requires being in parent repo ('pwd' must return up to directory '/Rosalind_Bioinformatics/Bioinformatics_Stronghold')
-    FILEPATHREAD = "./datasets/P26_PMCH-sample.txt"
-    # FILEPATHREAD = "./datasets/P26_PMCH-dataset.txt"
+    FILEPATHREAD = "./datasets/P26_PMCH-dataset.txt"
     FILEPATHWRITE = "./outputs/P26_PMCH-output.txt"
 
     # Reads text data from raw dataset
     with open(FILEPATHREAD, "r") as fr:
-        data = fr.read()
+        data = "".join(fr.readlines()[1])
     
+    return print(calculate_perfect_matchings(data))
+
     # Creates output file and writes appropriate response to file and notifies user
     with open(FILEPATHWRITE, "w") as fw:
-        fw.write(str(data))
+        fw.write(str(calculate_perfect_matchings(data)))
 
     return print("\nThe Perfect Matchings dataset has been processed and the appropriate output has been saved to {}.\n".format(FILEPATHWRITE[2:]))
 
